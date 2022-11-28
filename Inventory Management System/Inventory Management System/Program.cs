37000b0a484c5d5b4cd59e552764e993dcf5efc1
Space987 <82058206+Space987@users.noreply.github.com>
2022-11-28 09:29:53 -0500

    Add stuffs

--- Inventory Management System/Inventory Management System/Program.cs
+++ Inventory Management System/Inventory Management System/Program.cs
@@ -14,7 +14,7 @@ namespace Inventory_Management_System
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new ProductDetails()); 
+            //Application.Run(new ProductDetails());
             Application.Run(new CreateProduct());   
         }
     }
